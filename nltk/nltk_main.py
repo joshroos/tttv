8be9f257dcfa417201a6622a215fa7e888230d7e
Joshua de Roos <joshua_deroos@hotmail.com>
2019-04-29 12:39:55 +0200

    hoi

--- nltk/nltk_main.py
+++ nltk/nltk_main.py
@@ -3,10 +3,27 @@ from nltk import word_tokenize
 from nltk import sent_tokenize
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet
+
+
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
+
 
 def get_words(txt):
     regex = RegexpTokenizer(r'\w+')
     return [w.lower() for w in regex.tokenize(txt)]
+
 
 def get_sentences(txt):
     return sent_tokenize(txt)
@@ -14,8 +31,8 @@ def get_sentences(txt):
 
 def lemmatize_words(words):
     lemmatizer = WordNetLemmatizer()
-    word = 'times'
-    lemmas = lemmatizer.lemmatize(word, 'v')
+    print(words)
+    lemmas = lemmatizer.lemmatize(words[0], get_wordnet_pos(words[1]))
     print(lemmas)
 
 
@@ -24,33 +41,51 @@ def get_unknown_words(words):
 
     return [w for w in words if not w in d]
 
+
 def avg_len_words(words):
     return sum([len(w) for w in words]) / len(words)
+
 
 def avg_len_sent(sentences):
     length = 0
     for sent in sentences:
-        length = len(get_words(sent))
-        if length > 40:
-            print(nltk.pos_tag(get_words(sent)))
+        length += len(get_words(sent))
     return length / len(sentences)
+
 
 def hapaxes(words):
     return [w for w in words if words.count(w) == 1 ]
 
+
+def pos_tags(sentences):
+    used_tags = []
+    for sent in sentences:
+        words = get_words(sent)
+        pos = nltk.pos_tag(words)
+
+        for i in pos:
+            if i[1] not in used_tags:
+                used_tags.append(i[1])
+    return used_tags
+
 txt = open('antonio.txt').read()
 words = get_words(txt)
 sentences = get_sentences(txt)
+tags = sorted(pos_tags(sentences))
 
 print("--Text Statistics --")
 print("# words: \t", len(words))
 print("# unique words: \t", len(set(words)))
 print("average words length: \t", round(avg_len_words(words), 2))
+print("unknown words: \t", get_unknown_words(set(words)))
 print("hapaxes: \t", hapaxes(words))
+print("tags: \t", tags)
+print("# tags: \t", len(tags))
 print("-------------------------------")
-print("# sentences: \t", len(sentences ))
+print("# sentences: \t", len(sentences))
 print("average sentence length: \t", round(avg_len_sent(sentences), 2))
-print(get_unknown_words(set(words)))
+print("longest sentence: \t", max([len(get_words(s)) for s in sentences]))
+
 
 
 # tokens = word_tokenize(txt)
